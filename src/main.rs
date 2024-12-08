6087373849c31d417a0f4dbd1f6369e88fe0b625
Hein Dauven <heindauven@gmail.com>
2024-12-08 01:26:02 +0100

    Add dummy allocator

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,27 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
+extern crate alloc;
+
+use alloc::boxed::Box;
+use blog_os::{memory, println};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::VirtAddr;
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     blog_os::init();
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    let x = Box::new(41);
 
     #[cfg(test)]
     test_main();
