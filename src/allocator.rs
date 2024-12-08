390267a4e24021217d4fd566317a10a9f8aa47a0
Hein Dauven <heindauven@gmail.com>
2024-12-08 13:23:10 +0100

    Add fixed size block alloc

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,9 +1,11 @@
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 use alloc::alloc::{GlobalAlloc, Layout};
 use bump::BumpAllocator;
 use core::ptr::null_mut;
+use fixed_size_block::FixedSizeBlockAllocator;
 use linked_list::LinkedListAllocator;
 use x86_64::{
     structures::paging::{
@@ -13,7 +15,7 @@ use x86_64::{
 };
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub struct Dummy;
 
